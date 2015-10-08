71c5e4c68cc525742659aee940c3b089366af8cb
grebsor <rosberg86@gmail.com>
2015-10-08 14:30:46 +0200

    Lägger till Repository

---
+++ Main.py
@@ -0,0 +1,49 @@
+
+'------------------------ Funktioner ------------------------'
+
+def bubbleSortINT(a):
+    'a=matris'
+
+def bubbleSortSTR(a):
+    'a=matris'
+    for x in a:
+        x[0][0]
+
+'------------------------   Main     ------------------------'
+b=True
+while b:
+    print "Välj vilket alternativ du vill göra "
+    print ""
+    print "1. Använd en färdig databas."
+    print "2. Skapa en databas."
+    print "3. Ta bort element ur databasen."
+    print "4. Lägg till element till databasen."
+    print "5. Skriv ut alla namn ur databasen."
+    print "6. Skriv ut alla åldrar ur databasen."
+    print "7. Skriv ut hela listan"
+    print "x. bla bla bla", "\n", "\n"
+    try:
+        val=int(raw_input("Ditt val 0-9: "))
+
+        if val==1:
+            l=[["Robin Rosberg", 29],["Eda Krodguldu", 19],["Stefan Falk", 49],["Knugen", 70],["Stina Svensson", 17]]
+            print "Här är den förberedda listan:", "\n", "\n", l, "\n", "\n" 
+        elif val==5:
+            for x in l:
+                print x[0]
+        elif val==6:
+            for x in l:
+                print x[1]
+        elif val==7:
+            for x in l:
+                print x[0],"   ",x[1]
+        else:
+            print "Du har inte gjort ett giltigt val. \n"
+        
+        val2=str(raw_input("Vill du göra ett nytt val? (y/n): "))
+        if val2=="n":
+            b=False
+        print "\n \n"
+    except:
+        print "Försök igen \n"
+        
